973a94ffaded6d0a3623928b999a363acfa3737f
David Park <hyunje.park@mail.utoronto.ca>
2024-11-14 17:18:54 -0500

    fixed simulation and test data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -20,59 +20,20 @@ obs = 100
 #### Simulate data ####
 simulated_data <-
   tibble(
-    "Income_1" = sample(1:1603, obs, replace = TRUE),
-    "Income_2" = sample(1:365, obs, replace = TRUE),
-    "Income_3" = sample(1:337, obs, replace = TRUE),
-    "Income_4" = sample(1:157, obs, replace = TRUE),
-    "Income_5" = sample(1:8, obs, replace = TRUE),
-    "Income_6" = sample(1:480, obs, replace = TRUE),
-    "Income_7" = sample(1:48, obs, replace = TRUE),
-    "Income_8" = sample(1:121, obs, replace = TRUE),
-    "Income_9" = sample(1:43, obs, replace = TRUE),
-    "Income_10" = sample(1:45, obs, replace = TRUE),
-    "Income_11" = sample(1:4, obs, replace = TRUE),
-    "Expense_1" = sample(1:24, obs, replace = TRUE),
-    "Expense_2" = sample(1:81, obs, replace = TRUE),
-    "Expense_3" = sample(1:3146, obs, replace = TRUE),
-    "Expense_4" = sample(1:705, obs, replace = TRUE),
-    "Expense_5" = sample(1:360, obs, replace = TRUE),
-    "Expense_6" = sample(1:460, obs, replace = TRUE),
+    "count" = sample(1:25000, obs, replace = TRUE),
   )
 
-# ensure all income/expense columns exist
+# ensure all item columns exist
 simulated_data
 names(simulated_data)
 
-# get the sum of income and expenses
-simulated_data$"sum_income" <- rowSums(subset(simulated_data, select = c("Income_1",
-                                                                     "Income_2",
-                                                                     "Income_3",
-                                                                     "Income_4",
-                                                                     "Income_5",
-                                                                     "Income_6",
-                                                                     "Income_7",
-                                                                     "Income_8",
-                                                                     "Income_9",
-                                                                     "Income_10",
-                                                                     "Income_11"
+# get the sum of item count
+simulated_data$"sum_income" <- rowSums(subset(simulated_data, select = c("count"
 )))
 
-simulated_data$"sum_expenses" <- rowSums(subset(simulated_data, select = c("Expense_1",
-                                                                       "Expense_2",
-                                                                       "Expense_3",
-                                                                       "Expense_4",
-                                                                       "Expense_5",
-                                                                       "Expense_6"
-)))
-
-# Get the difference between the sum of income and expenses
-simulated_data$"difference" <- (simulated_data$"sum_income" - simulated_data$"sum_expenses")
-simulated_data %>% select("sum_income", "sum_expenses", "difference")
-
-# FINAL TEST - there should be more negative rows than positive rows
-# (this is the assumption from paper.qmd, that is being tested)
-# (implies people cannot financially survive in toronto)
-print(sum(simulated_data$difference < 0) > sum(simulated_data$difference >= 0))
+# FINAL TEST - should be greater than 0
+# implying there vendors actually have items
+print(sum(simulated_data$count > 0))
 
 
 
--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -18,26 +18,16 @@ library(testthat)
 
 # get the cleaned dataset
 cleaned_data = read_csv(here::here('data/cleaned_data/cleaned_data.csv'))
-cleaned_data_inverted = read_csv(here::here('data/cleaned_data/cleaned_data_inverted.csv'))
+
 
 #### Test cleaned_data ####
 ### NEED TO TEST INCOME/EXPENSE IS PROPER ###
 
 # test 1 - test that it has all the columns
 test_that("Has all the columns",
-          {expected = c("Category",
-                       "Item",
-                       "Household_1",
-                       "Household_2",
-                       "Household_3",
-                       "Household_4",
-                       "Household_5",
-                       "Household_6",
-                       "Household_7",
-                       "Household_8",
-                       "Household_9",
-                       "Household_10",
-                       "Household_11")
+          {expected = c("vendor",
+                        "vendor_count"
+                       )
           expect_equal(colnames(cleaned_data), expected)})
 
 # test that the dataframe contains no NA values
@@ -47,28 +37,6 @@ test_that("No NA values",
 # test that all the Income sources are positive values
 # (cant have negative Income)
 test_that("All Income variables are not negative",
-          {
-          expect_true(all(cleaned_data_inverted$"Income 1" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 2" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 3" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 4" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 5" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 6" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 7" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 8" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 9" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 10" >= 0))
-          expect_true(all(cleaned_data_inverted$"Income 11" >= 0))
-          }
-          )
+          {expect_true(all(cleaned_data_inverted$"vendor_count" >= 0))})
+          
 
-cleaned_data_inverted
-# test that all Expenses are positive values
-test_that("All Expense variables are not negative",
-          {
-            expect_true(all(cleaned_data_inverted$"Expense 3" >= 0))
-            expect_true(all(cleaned_data_inverted$"Expense 4" >= 0))
-            expect_true(all(cleaned_data_inverted$"Expense 5" >= 0))
-            expect_true(all(cleaned_data_inverted$"Expense 6" >= 0))
-          }
-)
